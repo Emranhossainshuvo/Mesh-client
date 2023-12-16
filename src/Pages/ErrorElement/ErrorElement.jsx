1dd96112e63e68f7f5e11e8dba1e955c180b7646
Emranhossainshuvo <shuvoorba28@gmail.com>
2023-12-16 18:15:13 +0600

    erro element page added

---
+++ src/Pages/ErrorElement/ErrorElement.jsx
@@ -0,0 +1,22 @@
+
+import { Link } from "react-router-dom";
+
+const ErrorElement = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded shadow-md">
+        <h1 className="text-4xl font-bold mb-6 text-red-500">Error</h1>
+        <p className="text-gray-700 mb-6">
+          Oops! Something went wrong. Please try again later.
+        </p>
+        <Link>
+          <button className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 focus:outline-none focus:shadow-outline-red">
+            Home
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorElement;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,11 +3,13 @@ import {
   } from "react-router-dom";
 import Main from "../Layouts/Main";
 import Home from "../Pages/Home/Home/Home";
+import ErrorElement from "../Pages/ErrorElement/ErrorElement";
 
 export const router = createBrowserRouter([
     {
       path: "/",
       element: <Main></Main>,
+      errorElement: <ErrorElement></ErrorElement>,
       children: [
         {
             path: '/', 
