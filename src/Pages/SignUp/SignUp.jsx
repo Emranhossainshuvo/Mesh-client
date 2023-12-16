2974b72c1b055d9cc3cea2132e4eac7995affe1a
Emranhossainshuvo <shuvoorba28@gmail.com>
2023-12-16 19:21:27 +0600

    sign up page created and toggle functionality added between login and sign up page

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-const Login = () => {
+const SignUp = () => {
   return (
     <>
       <section className="bg-gray-50 dark:bg-gray-900">
@@ -19,7 +19,7 @@ const Login = () => {
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Sign in to your account
+                Create and account
               </h1>
               <form className="space-y-4 md:space-y-6" action="#">
                 <div>
@@ -30,7 +30,7 @@ const Login = () => {
                     type="email"
                     name="email"
                     id="email"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-[#3F2305] focus:border-[#3F2305] block w-full p-2.5 dark:bg-gray-700 dark:border-[#3F2305] dark:placeholder-gray-400 dark:text-white dark:focus:ring-[#3F2305] dark:focus:border-[#3F2305]"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="name@company.com"
                     required=""
                   />
@@ -44,48 +44,59 @@ const Login = () => {
                     name="password"
                     id="password"
                     placeholder="••••••••"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-[#3F2305] focus:border-[#3F2305] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-[#3F2305] dark:focus:border-[#3F2305]"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required=""
                   />
                 </div>
-                <div className="flex items-center justify-between">
-                  <div className="flex items-start">
-                    <div className="flex items-center h-5">
-                      <input
-                        id="remember"
-                        aria-describedby="remember"
-                        type="checkbox"
-                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-[#3F2305] dark:ring-offset-gray-800"
-                        required=""
-                      />
-                    </div>
-                    <div className="ml-3 text-sm">
-                      <label className="text-gray-500 dark:text-gray-300">
-                        Remember me
-                      </label>
-                    </div>
+                <div>
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Confirm password
+                  </label>
+                  <input
+                    type="confirm-password"
+                    name="confirm-password"
+                    id="confirm-password"
+                    placeholder="••••••••"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required=""
+                  />
+                </div>
+                <div className="flex items-start">
+                  <div className="flex items-center h-5">
+                    <input
+                      id="terms"
+                      aria-describedby="terms"
+                      type="checkbox"
+                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                      required=""
+                    />
                   </div>
-                  <a
-                    href="#"
-                    className="text-sm font-medium text-[#3F2305] hover:underline dark:text-[#3F2305]"
-                  >
-                    Forgot password?
-                  </a>
+                  <div className="ml-3 text-sm">
+                    <label className="font-light text-gray-500 dark:text-gray-300">
+                      I accept the{" "}
+                      <a
+                        className="font-medium text-[#3F2305] hover:underline dark:text-[#3F2305]"
+                        href="#"
+                      >
+                        Terms and Conditions
+                      </a>
+                    </label>
+                  </div>
                 </div>
                 <button
                   type="submit"
                   className="w-full text-white bg-[#3F2305] hover:bg-[#3F2305] focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-[#3F2305] dark:hover:bg-[#3F2305] dark:focus:ring-[#3F2305]"
                 >
-                  Sign in
+                  Create an account
                 </button>
-                <Link to='/signup'>
+                <Link to='/login'>
                   <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                    Don’t have an account yet?{" "}
+                    Already have an account?{" "}
                     <a
                       href="#"
                       className="font-medium text-[#3F2305] hover:underline dark:text-[#3F2305]"
                     >
-                      Sign up
+                      Login here
                     </a>
                   </p>
                 </Link>
@@ -98,4 +109,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default SignUp;
