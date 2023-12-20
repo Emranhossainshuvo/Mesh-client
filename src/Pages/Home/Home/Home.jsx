3c2bda8db6ec18bd3da246ebcaddcdb33eff5e31
Emranhossainshuvo <shuvoorba28@gmail.com>
2023-12-20 15:25:37 +0600

    we provide section added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,6 +1,7 @@
 import { Helmet } from "react-helmet-async";
 import Banner from "../Banner/Banner";
 import JustForYou from "../JustForYou/JustForYou";
+import WeProvide from "../WeProvide/WeProvide";
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
         <Helmet>
             <title>Mesh | Home</title>
         </Helmet>
+        <WeProvide></WeProvide>
         <JustForYou></JustForYou>
             <p>This is home</p>            
         </>
---
+++ src/Pages/Home/WeProvide/WeProvide.jsx
@@ -0,0 +1,113 @@
+import SectionTitle from "../../../SectionTitle/SectionTitle";
+
+const WeProvide = () => {
+  return (
+    <div className="max-w-7xl mx-auto">
+      <SectionTitle title="We provide"></SectionTitle>
+
+      <div className="w-full mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+       
+        
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure className="px-10 pt-10">
+            <img
+              src="https://i.ibb.co/jkkGZy0/kelly-sikkema-OG9p-Ie51rh-U-unsplash.jpg"
+              alt="Shoes"
+              className="rounded-xl"
+            />
+          </figure>
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">Pastels</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure className="px-10 pt-10">
+            <img
+              src="https://i.ibb.co/3p1kzt3/animated-eraser-painting-easel.jpg"
+              alt="Shoes"
+              className="rounded-xl"
+            />
+          </figure>
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">Canvas & Surfaces</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure className="px-10 pt-10">
+            <img
+              src="https://i.ibb.co/fvSXGJ0/kelli-tungay-2-LJ4rq-K2qf-U-unsplash.jpg"
+              alt="Shoes"
+              className="rounded-xl"
+            />
+          </figure>
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">Pencils</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure className="px-10 pt-10">
+            <img
+              src="https://i.ibb.co/LYqv99m/raychan-k-JQ6c-Dyod-AM-unsplash.jpg"
+              alt="Shoes"
+              className="rounded-xl"
+            />
+          </figure>
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">Easels</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure className="px-10 pt-10">
+            <img
+              src="https://i.ibb.co/zmyqQF4/steve-johnson-vcoc8e9-Wbi-A-unsplash.jpg"
+              alt="Shoes"
+              className="rounded-xl"
+            />
+          </figure>
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">Palette Knives</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure className="px-10 pt-10">
+            <img
+              src="https://i.ibb.co/XFYSmzv/kelly-sikkema-10-AH6f-V6-Iz8-unsplash.jpg"
+              alt="Shoes"
+              className="rounded-xl"
+            />
+          </figure>
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">Markers</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WeProvide;
